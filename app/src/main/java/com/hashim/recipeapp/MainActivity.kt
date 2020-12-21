381c69d804486a8ddca5e04e5a4740db9c3f2fff
hashim <hashimtahir@live.com>
2020-12-22 02:10:24 +0500

    First test completed with jetpack compose.

--- app/src/main/java/com/hashim/recipeapp/MainActivity.kt
+++ app/src/main/java/com/hashim/recipeapp/MainActivity.kt
@@ -4,12 +4,30 @@
 
 package com.hashim.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                    modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look Some Text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "A Button")
+                }
+            }
+        }
     }
 }
