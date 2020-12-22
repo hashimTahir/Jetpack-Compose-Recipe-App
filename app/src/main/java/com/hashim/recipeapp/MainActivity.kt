240ae2679b753e3861acb605da49f602f115766a
hashim <hashimtahir@live.com>
2020-12-22 12:41:08 +0500

    Row column example with alignment.

--- app/src/main/java/com/hashim/recipeapp/MainActivity.kt
+++ app/src/main/java/com/hashim/recipeapp/MainActivity.kt
@@ -6,67 +6,56 @@ package com.hashim.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.imageFromResource
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ScrollableColumn(
-                modifier = Modifier.background(
-                    color = Color(0xFFF2F2F2)
-                )
-                    .fillMaxHeight()
-            ) {
-                Image(
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop,
-                    bitmap = imageFromResource(
-                        res = resources,
-                        resId = R.drawable.happy_meal
-                    )
-                )
+            Column {
                 Column(
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+                    verticalArrangement = Arrangement.Center
                 ) {
                     Text(
-                        text = "Happy Meal",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(26)
-                        )
+                        text = "This is item 1",
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
                     )
-
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
                     Text(
-                        text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
+                        text = "This is item 2",
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
                     )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                }
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Row(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(200.dp)
+                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+                    horizontalArrangement = Arrangement.Center
+                ) {
                     Text(
-                        text = "$5.99",
-                        style = TextStyle(
-                            color = Color(0xFF85bb64),
-                            fontSize = TextUnit.Companion.Sp(10)
-                        )
+                        text = "This is item 1",
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterVertically)
                     )
-
-
                 }
             }
+
 
         }
     }
