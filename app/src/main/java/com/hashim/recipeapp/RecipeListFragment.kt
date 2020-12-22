56d139d77ff2fe3dfe7ae7e229ca6aef9c21db25
hashim <hashimtahir@live.com>
2020-12-22 14:46:08 +0500

    Set composeview inside a xml layout file.

--- app/src/main/java/com/hashim/recipeapp/RecipeListFragment.kt
+++ app/src/main/java/com/hashim/recipeapp/RecipeListFragment.kt
@@ -8,8 +8,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
 class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
@@ -19,15 +28,29 @@ class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        val hView = inflater.inflate(R.layout.fragment_recipe_list, container, false)
 
-        return  ComposeView(
-            requireContext()
-        ).apply {
-            setContent {
-                Text("Compose RecipeListFragment")
+        hView.findViewById<ComposeView>(R.id.hComposeView)
+            .setContent {
+                Column(
+                    modifier = Modifier.border(
+                        border = BorderStroke(1.dp, Color.Black)
+                    )
+                        .padding(16.dp)
+                ) {
+
+                    Text("This is compose inside fragment layout")
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    CircularProgressIndicator()
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text("New Text")
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+
+                }
             }
-        }
+        return hView
     }
 
 }
