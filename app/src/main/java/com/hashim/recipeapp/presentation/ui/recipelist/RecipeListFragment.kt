52b667e529883c98a6c0985fefcc6ce37dcc609d
hashim <hashimtahir@live.com>
2020-12-24 16:43:08 +0500

    Custom Compose Card created and added in the RecipeListFragment

--- app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
@@ -8,26 +8,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
 import com.google.gson.Gson
 import com.hashim.recipeapp.R
+import com.hashim.recipeapp.presentation.ui.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -45,26 +33,13 @@ class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-
                 val value = hRecipeListViewModel.hRecipeListMS.value
-                Timber.d("Data List %s", hGson.toJson(value))
-                Column(
-                    modifier = Modifier
-                        .border(border = BorderStroke(1.dp, Color.Black))
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "Recipe List fragment",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(21)
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
 
-                    Button(onClick = {
-                        findNavController().navigate(R.id.action_hRecipeListFragment_to_hReciepeFragment)
-                    }) {
-                        Text(text = "To Recipe Fragment")
+                LazyColumn {
+                    itemsIndexed(
+                        items = value
+                    ) { index, recipe ->
+                        RecipeCard(recipe = recipe, onclick = {})
                     }
                 }
             }
--- app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListViewModel.kt
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListViewModel.kt
@@ -12,7 +12,6 @@ import androidx.lifecycle.viewModelScope
 import com.hashim.recipeapp.domain.model.Recipe
 import com.hashim.recipeapp.repository.RecipeRepositoryImpl
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Named
 
 
@@ -20,13 +19,13 @@ class RecipeListViewModel @ViewModelInject constructor(
     private val hRecipeRepository: RecipeRepositoryImpl,
     private @Named("auth_token") val hToken: String
 ) : ViewModel() {
-    val hRecipeListMS: MutableState<List<Recipe>> = mutableStateOf(listOf())
+    val hRecipeListMS: MutableState<List<Recipe>> = mutableStateOf(ArrayList())
 
     init {
+        hNewSearch()
+    }
 
-        Timber.d("Repository %s", hRecipeRepository)
-        Timber.d("Token %s", hToken)
-
+    private fun hNewSearch() {
         viewModelScope.launch {
             val hSearch = hRecipeRepository.hSearch(
                 token = hToken,
