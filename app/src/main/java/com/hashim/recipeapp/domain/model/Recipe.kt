00eb3c12307e9d9c9259197bce797b26f452c973
hashim <hashimtahir@live.com>
2020-12-24 09:30:28 +0500

    Domain model added.

---
+++ app/src/main/java/com/hashim/recipeapp/domain/model/Recipe.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020/  12/ 24.  Created by Hashim Tahir
+ */
+
+package com.hashim.recipeapp.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Recipe(
+    val id: Int? = null,
+    val title: String? = null,
+    val publisher: String? = null,
+    val featuredImage: String? = null,
+    val rating: Int? = 0,
+    val sourceUrl: String? = null,
+    val description: String? = null,
+    val cookingInstructions: String? = null,
+    val ingredients: List<String> = listOf(),
+    val dateAdded: String? = null,
+    val dateUpdated: String? = null,
+) : Parcelable
