88cd5640bf2b321235a60a15a725e9e1ff088e5e
hashim <hashimtahir@live.com>
2020-12-30 22:36:44 +0500

    StateHoisting implemented for the SearchAppbar
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/components/SearchAppbar.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2020/  12/ 30.  Created by Hashim Tahir
+ */
+
+package com.hashim.recipeapp.presentation.ui.components
+
+import androidx.compose.foundation.ScrollableRow
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.hashim.recipeapp.presentation.ui.recipelist.FoodCategory
+import com.hashim.recipeapp.presentation.ui.recipelist.hGetAllFoodCategories
+
+@Composable
+fun SearchAppBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+    scrollPostion: Float,
+    selectedCategory: FoodCategory?,
+    onSelectedCategoryChanged: (String) -> Unit,
+    onSetCategoryScroolPosition: (Float) -> Unit
+) {
+    Surface(
+        elevation = 8.dp,
+        modifier = Modifier.fillMaxWidth(),
+        color = Color.White
+    ) {
+
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                TextField(
+                    value = query,
+                    onValueChange = { newValue ->
+                        onQueryChanged(newValue)
+                    },
+                    modifier = Modifier.fillMaxWidth(0.9f)
+                        .padding(8.dp),
+                    label = {
+                        Text(text = "Search")
+                    },
+                    leadingIcon = {
+                        Icon(Icons.Filled.Search)
+                    },
+                    onImeActionPerformed = { action, softKeyboardController ->
+                        if (action == ImeAction.Search) {
+                            onExecuteSearch()
+                            softKeyboardController?.hideSoftwareKeyboard()
+
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    ),
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    backgroundColor = MaterialTheme.colors.surface,
+                )
+            }
+
+            val hScrollState = rememberScrollState()
+
+            ScrollableRow(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 8.dp, bottom = 8.dp),
+                scrollState = hScrollState
+            ) {
+                hScrollState.scrollTo(scrollPostion)
+                for (category in hGetAllFoodCategories()) {
+                    FoodCategoryChip(
+                        category = category.value,
+                        isSelected = selectedCategory == category,
+                        onSelectedCategoryChanged = {
+                            onSelectedCategoryChanged(it)
+                            onSetCategoryScroolPosition(hScrollState.value)
+                        },
+                        onExecuteSearch = {
+                            onExecuteSearch()
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
@@ -8,29 +8,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.ScrollableRow
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.gson.Gson
-import com.hashim.recipeapp.presentation.ui.components.FoodCategoryChip
 import com.hashim.recipeapp.presentation.ui.components.RecipeCard
+import com.hashim.recipeapp.presentation.ui.components.SearchAppBar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -55,71 +40,15 @@ class RecipeListFragment : Fragment() {
                 val selectedCategory = hRecipeListViewModel.hSelectedCategory.value
 
                 Column {
-                    Surface(
-                        elevation = 8.dp,
-                        modifier = Modifier.fillMaxWidth(),
-                        color = Color.White
-                    ) {
-
-                        Column {
-                            Row(
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                TextField(
-                                    value = query,
-                                    onValueChange = { newValue ->
-                                        hRecipeListViewModel.hOnQueryChanged(newValue)
-                                    },
-                                    modifier = Modifier.fillMaxWidth(0.9f)
-                                        .padding(8.dp),
-                                    label = {
-                                        Text(text = "Search")
-                                    },
-                                    leadingIcon = {
-                                        Icon(Icons.Filled.Search)
-                                    },
-                                    onImeActionPerformed = { action, softKeyboardController ->
-                                        if (action == ImeAction.Search) {
-                                            hRecipeListViewModel.hNewSearch()
-                                            softKeyboardController?.hideSoftwareKeyboard()
-
-                                        }
-                                    },
-                                    keyboardOptions = KeyboardOptions(
-                                        keyboardType = KeyboardType.Text,
-                                        imeAction = ImeAction.Search
-                                    ),
-                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                    backgroundColor = MaterialTheme.colors.surface,
-                                )
-
-                            }
-
-                            val hScrollState = rememberScrollState()
-
-                            ScrollableRow(
-                                modifier = Modifier.fillMaxWidth()
-                                    .padding(start = 8.dp, bottom = 8.dp),
-                                scrollState = hScrollState
-                            ) {
-                                hScrollState.scrollTo(hRecipeListViewModel.hCategoryScroolPostion)
-                                for (category in hGetAllFoodCategories()) {
-                                    FoodCategoryChip(
-                                        category = category.value,
-                                        isSelected = selectedCategory == category,
-                                        onSelectedCategoryChanged = {
-                                            hRecipeListViewModel.hOnSelectedCategoryChanged(category = it)
-                                            hRecipeListViewModel.hOnSetCategoryScroolPosition(hScrollState.value)
-                                        },
-                                        onExecuteSearch = {
-                                            hRecipeListViewModel.hNewSearch()
-                                        }
-                                    )
-                                }
-
-                            }
-                        }
-                    }
+                    SearchAppBar(
+                        query = query,
+                        onQueryChanged = { hRecipeListViewModel::hOnQueryChanged },
+                        onExecuteSearch = { hRecipeListViewModel::hNewSearch },
+                        scrollPostion = hRecipeListViewModel.hCategoryScroolPostion,
+                        selectedCategory = selectedCategory,
+                        onSelectedCategoryChanged = { hRecipeListViewModel::hOnSelectedCategoryChanged },
+                        onSetCategoryScroolPosition = { hRecipeListViewModel::hOnSetCategoryScroolPosition }
+                    )
                     LazyColumn {
                         itemsIndexed(
                             items = value
