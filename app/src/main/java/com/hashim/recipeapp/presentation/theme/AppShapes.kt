60959cb77711ff28af438e5d4db9d98bf3eac9d2
hashim <hashimtahir@live.com>
2021-01-09 11:50:48 +0500

    Shapes and fonts added.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/recipeapp/presentation/theme/AppShapes.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021/  1/ 9.  Created by Hashim Tahir
+ */
+
+package com.hashim.recipeapp.presentation.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(6.dp),
+    large = RoundedCornerShape(8.dp)
+)
