238a172dca3ecbc3069a6179ce76eb779caf5fc9
hashim <hashimtahir@live.com>
2021-01-10 13:49:46 +0500

    Scaffold and BottomBar examples added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
@@ -10,10 +10,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
@@ -22,10 +22,7 @@ import androidx.fragment.app.viewModels
 import com.google.gson.Gson
 import com.hashim.recipeapp.presentation.BaseApplication
 import com.hashim.recipeapp.presentation.theme.AppTheme
-import com.hashim.recipeapp.presentation.ui.components.CircularProgressBar
-import com.hashim.recipeapp.presentation.ui.components.LoadingRecipeListShimmer
-import com.hashim.recipeapp.presentation.ui.components.RecipeCard
-import com.hashim.recipeapp.presentation.ui.components.SearchAppBar
+import com.hashim.recipeapp.presentation.ui.components.*
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -56,29 +53,39 @@ class RecipeListFragment : Fragment() {
                     val categoryScrollPosition = hRecipeListViewModel.hCategoryScroolPostion
                     val hIsLoading = hRecipeListViewModel.hIsLoading.value
 
-                    Column {
-                        SearchAppBar(
-                            query = query,
-                            onQueryChanged = hRecipeListViewModel::hOnQueryChanged,
-                            onExecuteSearch = hRecipeListViewModel::hNewSearch,
-                            categories = hGetAllFoodCategories(),
-                            selectedCategory = selectedCategory,
-                            onSelectedCategoryChanged = hRecipeListViewModel::hOnSelectedCategoryChanged,
-                            scrollPosition = categoryScrollPosition,
-                            onChangeScrollPosition = hRecipeListViewModel::hOnSetCategoryScroolPosition,
-                            onToggleTheme = {
-                                hApplication.hToggleTheme()
-                            }
-                        )
+                    /*Scaffold is used to add top, bottom, and drawers like componets
+                    * with compose.
+                    * content goes in content function*/
 
+                    Scaffold(
+                        topBar = {
+                            SearchAppBar(
+                                query = query,
+                                onQueryChanged = hRecipeListViewModel::hOnQueryChanged,
+                                onExecuteSearch = hRecipeListViewModel::hNewSearch,
+                                categories = hGetAllFoodCategories(),
+                                selectedCategory = selectedCategory,
+                                onSelectedCategoryChanged = hRecipeListViewModel::hOnSelectedCategoryChanged,
+                                scrollPosition = categoryScrollPosition,
+                                onChangeScrollPosition = hRecipeListViewModel::hOnSetCategoryScroolPosition,
+                                onToggleTheme = {
+                                    hApplication.hToggleTheme()
+                                }
+                            )
+                        },
+                        bottomBar = {
+                            BottomAppBar()
+                        },
+                        drawerContent = {}
+                    ) {
                         /*Box is like a frame layout
-                        * children are stacked on top of one and other with priority from
-                        * top to bottom*/
+                                              * children are stacked on top of one and other with priority from
+                                              * top to bottom*/
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .background(
-                                    color = MaterialTheme.colors.background
+                                    color = MaterialTheme.colors.surface
                                 )
                         ) {
                             if (hIsLoading) {
