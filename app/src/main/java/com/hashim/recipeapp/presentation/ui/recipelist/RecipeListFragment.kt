3d3618ebdf3d81741662b2730e9433c53cbe9f87
hashim <hashimtahir@live.com>
2021-01-11 23:25:39 +0500

    Snackbar functionality fully implemented in HsnackBar.kt
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/hashim/recipeapp/presentation/ui/recipelist/RecipeListFragment.kt
@@ -8,16 +8,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.*
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.gson.Gson
 import com.hashim.recipeapp.presentation.BaseApplication
-import com.hashim.recipeapp.presentation.ui.components.DecoupledSnackBar
+import com.hashim.recipeapp.presentation.theme.AppTheme
+import com.hashim.recipeapp.presentation.ui.components.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -41,82 +50,95 @@ class RecipeListFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                val hSnackbarHostState = remember { SnackbarHostState() }
-                Column {
-                    Button(
-                        onClick = {
-                            lifecycleScope.launch {
-                                hSnackbarHostState.showSnackbar(
-                                    message = "A SanckBar",
-                                    actionLabel = "Hide",
-                                    duration = SnackbarDuration.Short
-                                )
-                            }
+
+
+                AppTheme(
+                    darkTheme = hApplication.hIsdark.value
+                ) {
+                    val value = hRecipeListViewModel.hRecipeListMS.value
+                    val query = hRecipeListViewModel.hQuery.value
+                    val selectedCategory = hRecipeListViewModel.hSelectedCategory.value
+                    val categoryScrollPosition = hRecipeListViewModel.hCategoryScroolPostion
+                    val hIsLoading = hRecipeListViewModel.hIsLoading.value
+
+                    /*Scaffold is used to add top, bottom, and drawers like componets
+                    * with compose.
+                    * content goes in content function*/
+
+                    val hScaffoldState = rememberScaffoldState()
+                    Scaffold(
+                        topBar = {
+                            SearchAppBar(
+                                query = query,
+                                onQueryChanged = hRecipeListViewModel::hOnQueryChanged,
+                                onExecuteSearch = {
+                                    if (hRecipeListViewModel.hSelectedCategory
+                                            .value?.value == "Milk"
+                                    ) {
+                                        lifecycleScope.launch {
+                                            hScaffoldState.snackbarHostState
+                                                .showSnackbar(
+                                                    message = "Invalid Category : Milk",
+                                                    actionLabel = "Hide",
+                                                )
+                                        }
+                                    } else {
+                                        hRecipeListViewModel.hNewSearch()
+                                    }
+                                },
+                                categories = hGetAllFoodCategories(),
+                                selectedCategory = selectedCategory,
+                                onSelectedCategoryChanged = hRecipeListViewModel::hOnSelectedCategoryChanged,
+                                scrollPosition = categoryScrollPosition,
+                                onChangeScrollPosition = hRecipeListViewModel::hOnSetCategoryScroolPosition,
+                                onToggleTheme = {
+                                    hApplication.hToggleTheme()
+                                }
+                            )
+                        },
+                        scaffoldState = hScaffoldState,
+                        snackbarHost = {
+                            hScaffoldState.snackbarHostState
                         }
                     ) {
-                        Text(text = "Show SnackBar")
+                        /*Box is like a frame layout
+                                              * children are stacked on top of one and other with priority from
+                                              * top to bottom*/
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(
+                                    color = MaterialTheme.colors.surface
+                                )
+                        ) {
+                            if (hIsLoading) {
+                                LoadingRecipeListShimmer(cardHeigt = 250.dp)
+
+                            } else {
+                                LazyColumn {
+                                    itemsIndexed(
+                                        items = value
+                                    ) { index, recipe ->
+                                        RecipeCard(recipe = recipe, onclick = {})
+                                    }
+                                }
+                            }
+
+                            CircularProgressBar(isDisplayed = hIsLoading)
+
+                            HsnackBar(
+                                snackbarHostState = hScaffoldState.snackbarHostState,
+                                onDismiss = {
+                                    hScaffoldState.snackbarHostState
+                                        .currentSnackbarData?.dismiss()
+                                },
+                                modifier = Modifier.align(
+                                    Alignment.BottomCenter
+                                )
+                            )
+                        }
                     }
-                    DecoupledSnackBar(snackbarHostState = hSnackbarHostState)
-
                 }
-
-//                AppTheme(
-//                    darkTheme = hApplication.hIsdark.value
-//                ) {
-//                    val value = hRecipeListViewModel.hRecipeListMS.value
-//                    val query = hRecipeListViewModel.hQuery.value
-//                    val selectedCategory = hRecipeListViewModel.hSelectedCategory.value
-//                    val categoryScrollPosition = hRecipeListViewModel.hCategoryScroolPostion
-//                    val hIsLoading = hRecipeListViewModel.hIsLoading.value
-//
-//                    /*Scaffold is used to add top, bottom, and drawers like componets
-//                    * with compose.
-//                    * content goes in content function*/
-//
-//                    Scaffold(
-//                        topBar = {
-//                            SearchAppBar(
-//                                query = query,
-//                                onQueryChanged = hRecipeListViewModel::hOnQueryChanged,
-//                                onExecuteSearch = hRecipeListViewModel::hNewSearch,
-//                                categories = hGetAllFoodCategories(),
-//                                selectedCategory = selectedCategory,
-//                                onSelectedCategoryChanged = hRecipeListViewModel::hOnSelectedCategoryChanged,
-//                                scrollPosition = categoryScrollPosition,
-//                                onChangeScrollPosition = hRecipeListViewModel::hOnSetCategoryScroolPosition,
-//                                onToggleTheme = {
-//                                    hApplication.hToggleTheme()
-//                                }
-//                            )
-//                        },
-//                    ) {
-//                        /*Box is like a frame layout
-//                                              * children are stacked on top of one and other with priority from
-//                                              * top to bottom*/
-//                        Box(
-//                            modifier = Modifier
-//                                .fillMaxSize()
-//                                .background(
-//                                    color = MaterialTheme.colors.surface
-//                                )
-//                        ) {
-//                            if (hIsLoading) {
-//                                LoadingRecipeListShimmer(cardHeigt = 250.dp)
-//
-//                            } else {
-//                                LazyColumn {
-//                                    itemsIndexed(
-//                                        items = value
-//                                    ) { index, recipe ->
-//                                        RecipeCard(recipe = recipe, onclick = {})
-//                                    }
-//                                }
-//                            }
-//
-//                            CircularProgressBar(isDisplayed = hIsLoading)
-//                        }
-//                    }
-//                }
 
             }
         }
